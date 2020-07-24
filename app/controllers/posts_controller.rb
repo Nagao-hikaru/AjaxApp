e49aee97cbf0f2b864d6a4f94ced699265aeab38
Nagao-hikaru <koyanagi29sushi@gmail.com>
2020-07-24 12:53:25 +0900

    jsonデータのレスポンス設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
   def new
@@ -9,5 +9,21 @@ class PostsController < ApplicationController
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
+  end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked then
+      # checkedメソッドは既読されていない時に真となる
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: {post: item}
+    # jsonファイルを返却する時には文法上キーバリューの形式で返しており
+    # keyになるのは名前はなんでもよくて値は取得したレコードをそのまま返す形とする。
   end
 end
